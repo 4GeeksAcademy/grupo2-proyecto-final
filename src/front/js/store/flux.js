637ec37617de49c8c70f8c736a15b8adaaec8346
giulianac-git <79769030+giulianac-git@users.noreply.github.com>
2023-08-27 00:38:28 +0000

    [Integrated login, signup frontend to endpoints]
    - src/front/js/layout.js: Added login and signup views
    - src/front/js/pages/Login.js: Created login frontend
    - src/front/js/pages/SignUp.js: Created signup frontend
    - src/front/js/store/flux.js: Created fetches for login, signup and
    restricted
    - src/front/styles/home.css: Imported login and signup styles
    - src/front/styles/login.css: Created login styles
    - src/front/styles/signup.css: Created signup styles

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,19 +1,16 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			viewSignUp: false,
+			viewLogged: false,
+
+			email: null,
+			password: null,
+			token: null,
+			loggedUser: "",
+
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			demo: null,
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -22,14 +19,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -46,9 +43,101 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+
+			login: async (email, password) => {
+				try {
+					const options = {
+						method: 'POST',
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							"email": email,
+							"password": password
+						})
+					};
+
+					const resp = await fetch(process.env.BACKEND_URL + "/api/login", options);
+					const data = await resp.json();
+					console.log(data);
+
+					if (resp.status == 200) {
+						localStorage.setItem("token", data.access_token)
+						setStore({ viewLogged: true })
+					} else alert(data.message);
+
+				} catch (err) {
+					console.error("An error has occurred during login", err);
+				}
+			},
+
+			signUp: async (email, password) => {
+				if (!email || !password) {
+					alert("Please enter both email and password");
+				}
+				try {
+					const options = {
+						method: 'POST',
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							"email": email,
+							"password": password
+						})
+					};
+
+					const resp = await fetch(process.env.BACKEND_URL + "/api/signup", options);
+					const data = await resp.json();
+					console.log(data);
+
+					if (resp.status == 200) {
+						alert("User registered successfully")
+						setStore({ viewSignUp: true })
+					} else {
+						alert(data.message);
+					}
+
+				} catch (err) {
+					console.error("An error has occurred during signup", err);
+				}
+			},
+
+			private: async () => {
+				const token = localStorage.getItem("token");
+				setStore({ token: token });
+
+				try {
+					const options = {
+						method: 'GET',
+						headers: {
+							"Content-Type": "application/json",
+							"Authorization": "Bearer " + token
+						}
+					};
+
+					const resp = await fetch(process.env.BACKEND_URL + "/api/private", options);
+					const data = await resp.json();
+					setStore({ loggedUser: data.User });
+					console.log(store.loggedUser);
+				}
+
+				catch (err) {
+					console.error("An error has occurred", err);
+				}
+			},
+
+			logout: () => {
+				localStorage.clear();
+				setStore({ viewLogged: false })
+			},
+
+			toggleSignUp: (value) => {
+				setStore({ viewSignUp: value })
+			},
 		}
-	};
+	}
 };
 
 export default getState;
