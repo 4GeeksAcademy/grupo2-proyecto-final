2c6a45ac2bb3737120915856dad550288b362bf2
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-08-31 02:57:49 +0000

    Missing image and watching providers

--- src/front/js/pages/SingleMovie.js
+++ src/front/js/pages/SingleMovie.js
@@ -1,11 +1,39 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Context } from '../store/appContext';
 import "../../styles/MovieView.css";
 
 const SingleMovie = () => {
     const { store, actions } = useContext(Context);
+    const [selectedMovie, setSelectedMovie] = useState("");
+    const API_key = "491c31c8a0eb95d5d79ed9ed60929455";
+    const movie_id = "00015";
     const params = useParams();
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                const response = await fetch(
+                    `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_key}`
+                );
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setSelectedMovie(data);
+                } else {
+                    console.error('Error fetching movie data');
+                }
+            } catch (error) {
+                console.error('Error fetching movie data', error);
+            }
+        };
+
+        fetchMovie();
+    }, []);
+
+    if (!selectedMovie) {
+        return <p>Oops...</p>;
+    }
 
     return (
         <>
@@ -20,13 +48,8 @@ const SingleMovie = () => {
                             />
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 mt-3">
-                            <h1 className="mt-5 text-white" >movie name</h1>
-                            <p className='text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
-                                quaerat dolorem corrupti incidunt eligendi, tenetur consectetur!
-                                Eaque asperiores maiores totam quaerat? Blanditiis eius
-                                necessitatibus beatae quam, ipsum quasi officiis vel! Rem saepe
-                                consectetur quam minima facere totam praesentium, illum ea ratione
-                                odit vel temporibus ad eos, quasi exercitationem.</p>
+                            <h1 className="mt-5 text-white" >{selectedMovie.title}</h1>
+                            <p className='text-white'>{selectedMovie.overview}</p>
                         </div>
                     </section>
                     <hr className='text-white' />
@@ -34,21 +57,23 @@ const SingleMovie = () => {
                         <section className="row text-center ">
                             <div className="col-lg-2 col-sm-2 col-6 text-white">
                                 <p className="fw-bold m-0">Release Date:</p>
+                                <p>{selectedMovie.release_date}</p>
                             </div>
                             <div className="col-lg-2 col-sm-2 col-6 text-white">
-                                <p className="fw-bold m-0">Translations:</p>
-                                {/* <p>{properties.}</p> */}
+                                <p className="fw-bold m-0">Original Language:</p>
+                                <p>{selectedMovie.original_language}</p>
                             </div>
                             <div className="col-lg-2 col-sm-2 col-6 text-white">
                                 <p className="fw-bold m-0">Genre:</p>
-
+                                <p>{selectedMovie.genres.name}</p>
                             </div>
                             <div className="col-lg-2 col-sm-2 col-6 text-white">
                                 <p className="fw-bold m-0">Watch Providers:</p>
-
+                                <p>{selectedMovie.original_language}</p>
                             </div>
                             <div className="col-lg-2 col-sm-2 col-6 text-white">
                                 <p className="fw-bold m-0">Ratings:</p>
+                                <p>{selectedMovie.vote_average}</p>
                             </div>
                         </section>
                     </div>
