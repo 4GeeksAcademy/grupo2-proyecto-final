c32405df77ff9e316043433f5bbf2274e9aba8ef
Giulianna Cersosimo <79769030+giulianac-git@users.noreply.github.com>
2023-09-14 19:31:17 +0000

    Added styles for Similar Movies

--- src/front/js/component/SimilarCard.js
+++ src/front/js/component/SimilarCard.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
+import '../../styles/SimilarCard.css'
 
 const SimilarCard = ({ movie }) => {
     const { store, actions } = useContext(Context);
@@ -8,12 +9,10 @@ const SimilarCard = ({ movie }) => {
         <div className="cardSimilar">
             <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                 <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} className="img-fluid" alt={movie.title} />
-                <Link to={`/movie/${movie.id}`} />
-            </div>
-            <div className="card-footer movies-footer">
-                <div className="movies-footer-title">
+                <div className="title-overlay">
                     {movie.title}
                 </div>
+                <Link to={`/movie/${movie.id}`} />
             </div>
         </div>
     )
